ba26ef4d7388c4209b819c5cd64d3e65ca2718d6
Md Sakib Anwar <sakibanwar1149095@gmail.com>
2022-09-28 18:01:30 -0400

    made changes to product list page for lab 5

--- Lab5/src/app/products.ts
+++ Lab5/src/app/products.ts
@@ -22,7 +22,19 @@ export const products = [
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: 'The most basic experience you can dream of'
+  },
+  {
+    id: 4,
+    name: 'Phone Pro Max',
+    price: 899,
+    description: 'The best of the best'
+  },
+  {
+    id: 5,
+    name: 'Headphone',
+    price: 99,
+    description: 'The best heaphones you don\'t own'
   }
 ];
 
