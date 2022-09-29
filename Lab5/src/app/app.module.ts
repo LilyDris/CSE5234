4ef92f3c323c7040be10042e861f6421fd61e8f5
Lily Driscoll <44526792+LilyDris@users.noreply.github.com>
2022-09-29 11:40:44 -0400

    Removed copyright as we are editing to make out own

--- Lab5/src/app/app.module.ts
+++ Lab5/src/app/app.module.ts
@@ -38,8 +38,4 @@ import { ShippingComponent } from './shipping/shipping.component';
 export class AppModule { }
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
+  
--- Lab5/src/polyfills.ts
+++ Lab5/src/polyfills.ts
@@ -54,8 +54,3 @@ import 'zone.js'; // Included with Angular CLI.
  */
 
 
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
