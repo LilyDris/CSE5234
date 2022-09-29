c2e84ce0db81a277b109aca91ba2e1314f2d5fd4
Lily Driscoll <44526792+LilyDris@users.noreply.github.com>
2022-09-29 11:12:16 -0400

    Fixed typo

--- Lab5/src/app/cart.service.ts
+++ Lab5/src/app/cart.service.ts
@@ -30,6 +30,6 @@ clearCart() {
 }
 
 getShippingPrices(){
-  return this.http.get<{type: string, price: number}[]>('/assets/shipping.json)');
+  return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
 }
 }
