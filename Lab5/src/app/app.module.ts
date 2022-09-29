fe7328e39f37fa0b34d81e248e37584c43679c4f
Md Sakib Anwar <sakibanwar1149095@gmail.com>
2022-09-29 14:31:05 -0400

    added payment component

--- Lab5/src/app/app.module.ts
+++ Lab5/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
 import { ShippingComponent } from './shipping/shipping.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   imports: [
@@ -30,6 +31,7 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductDetailsComponent,
     CartComponent,
     ShippingComponent,
+    PaymentComponent,
   ],
   bootstrap: [
     AppComponent
