c8154b306a43417d48d57bbdd4390c6277666eb9
Daniel Mills <danielmills3@outlook.com>
2022-10-02 20:24:47 -0400

    Summary page now has items

--- Lab5/src/app/cart.service.ts
+++ Lab5/src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Product } from './products';
+import { PaymentInfo } from './payment-info';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { totalmem } from 'os';
@@ -11,28 +12,38 @@ export class CartService {
   constructor(private http: HttpClient){
   }
   items: Product[] = [];
+  paymentInfo: PaymentInfo = {
+    CardNumber: 2,
+    CVV: 727,
+    ExpiryMonth: 1,
+    ExpiryYear: 2000
+  }
 
 /* . . . */
-addToCart(product: Product) {
-  this.items.push(product);
-}
-
-addToCartAmount(product: Product, amount: Number){
-  for(let i=0; i<amount ;i++) {
-   this.items.push(product);
+  addToCart(product: Product) {
+    this.items.push(product);
   }
-}
 
-getItems() {
-  return this.items;
-}
+  addToCartAmount(product: Product, amount: Number){
+    for(let i=0; i<amount ;i++) {
+    this.items.push(product);
+    }
+  }
 
-clearCart() {
-  this.items = [];
-  return this.items;
-}
+  getItems() {
+    return this.items;
+  }
 
-getShippingPrices(){
-  return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
-}
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices(){
+    return this.http.get<{type: string, price: number}[]>('/assets/shipping.json');
+  }
+
+  getPaymentInfo() {
+    return this.paymentInfo
+  }
 }
--- Lab5/src/app/summary/summary.component.ts
+++ Lab5/src/app/summary/summary.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.css']
 })
-export class SummaryComponent implements OnInit {
 
-  constructor() { }
+export class SummaryComponent {
 
-  ngOnInit(): void {
+  items = this.cartService.getItems();
+  total: number;
+  paymentInfo = this.cartService.getPaymentInfo();
+  
+  constructor(
+    private cartService: CartService
+  ) {
+    this.total = 0;
+   }
+
+  getTotal() {
+    for (let item of this.items) {
+      this.total += item.price
+    }
+    return this.total;
+  }
+
+  onSubmit(): void {
+    //Process checkout data here
+    this.items = this.cartService.clearCart();
+    window.alert('Your order has been submitted!')
   }
 
 }
